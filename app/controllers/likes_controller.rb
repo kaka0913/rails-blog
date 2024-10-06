cb5cf49ddc3d7d3a438e5d38af2c72085b7b81cb
kaka0913 <kabuyuu0913@gmail.com>
2024-10-06 13:15:52 +0900

    関連コントローラへのユーザの追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
   def create
     @comment = Comment.find(params[:comment_id])
-    @like = @comment.likes.create
+    @like = @comment.likes.create(user: current_user)
     redirect_to article_path(@comment.article)
   end
 
   def destroy
-    @like = Like.find(params[:id])
+    @like = current_user.likes.fund(params[:id])
     @comment = @like.comment
     @like.destroy
     redirect_to article_path(@comment.article)
