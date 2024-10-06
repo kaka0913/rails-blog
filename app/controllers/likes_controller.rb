e6e31610326407aa2434065026adb4bab3ae5ea1
kaka0913 <kabuyuu0913@gmail.com>
2024-10-06 16:22:15 +0900

    ユーザのいいね機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,17 @@
 class LikesController < ApplicationController
   def create
     @comment = Comment.find(params[:comment_id])
-    @like = @comment.likes.create(user: current_user)
+
+    unless @comment.likes.exists?(user: current_user)
+      @like = @comment.likes.create(user: current_user)
+    end
     redirect_to article_path(@comment.article)
   end
 
   def destroy
-    @like = current_user.likes.find(params[:id])
+    @like = current_user.likes.find_by(comment_id: params[:comment_id])
     @comment = @like.comment
-    @like.destroy
+    @like.destroy if @like
     redirect_to article_path(@comment.article)
   end
 end
